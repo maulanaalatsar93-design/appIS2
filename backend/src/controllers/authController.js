19790f029f645f566128693d20cc5250248c7410
Maulana Cipta P <maulana@istek2.internal>
2026-08-07 17:37:15 +0800

    fix: ganti bcrypt dengan bcryptjs untuk fix error 500 di Vercel

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,5 @@
 import prisma from '../utils/prisma.js';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 const JWT_SECRET = process.env.JWT_SECRET || 'supersecret_istek2_key_fallback';
 
